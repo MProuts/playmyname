4109ca0df08c0b23721f0dd1c230077eeb05cde6
Michael Prouty <michael.prouty@gmail.com>
2014-05-07 20:22:42 -0400

    Add audiolet, start messing with it

--- js/play-my-name.js
+++ js/play-my-name.js
@@ -39,4 +39,30 @@ $(document).ready(function(){
 
   // Render voice
   voice.draw(ctx, stave);
+
 });
+
+function playExample() {
+  var Synth = function(audiolet, frequency) {
+
+    AudioletGroup.apply(this, [audiolet, 0, 1]);
+    this.sine = new Sine(this.audiolet, frequency);
+    this.modulator = new Saw(this.audiolet, 2 * frequency);
+    this.modulatorMulAdd = new MulAdd(this.audiolet, frequency / 2, frequency);
+    this.gain = new Gain(this.audiolet);
+
+    this.modulator.connect(this.modulatorMulAdd);
+    this.modulatorMulAdd.connect(this.sine);
+    this.envelope.connect(this.gain, 0, 1);
+    this.sine.connect(this.outputs[0]);
+  };
+  extend(Synth, AudioletGroup);
+
+  var AudioletApp = function() {
+    this.audiolet = new Audiolet();
+    var synth = new Synth(this.audiolet, 440);
+    synth.connect(this.audiolet.output);
+  };
+
+  this.audioletApp = new AudioletApp();
+};
